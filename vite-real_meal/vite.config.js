30f197d1c62c96969244372eaed15b99400748ae
Adrienx <71516635+Adrienx@users.noreply.github.com>
2023-06-29 00:41:18 -0400

    finally got deploy to surge working

--- vite-real_meal/vite.config.js
+++ vite-real_meal/vite.config.js
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite"
+import react from "@vitejs/plugin-react"
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  esbuild: {
+    jsxFactory: "_jsx",
+    jsxFragment: "_Fragment",
+    jsxInject:
+      "import { jsx as _jsx, jsxs as _jsxs, Fragment as _Fragment } from 'react'",
+  },
 })
